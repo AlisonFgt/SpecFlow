2247e4f402d79d7b548040151b19c91598e4f100
Alison Alves <alisonipuc@gmail.com>
2020-10-23 16:05:32 -0300

    first commit create project BDDSpecFlow

---
+++ BDDSpecFlow/Features/Calculator.feature
@@ -0,0 +1,11 @@
+﻿Feature: Calculator
+	A fim de evitar erros
+	Como um péssimo matemático
+	Eu quero que me digam a soma de dois números
+
+@mytag
+Scenario: Somar dois números
+	Given o primeiro numero é 50
+	And o segundo numero é 70
+	When os dois numeros sao adicionados
+	Then o resultado deve ser 120
---
+++ BDDSpecFlow/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,45 @@
+﻿using BDDSpecFlow.Example;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace BDDSpecFlow.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        private readonly ScenarioContext _scenarioContext;
+
+        private int result;
+
+        private Calculator calculator = new Calculator();        
+
+        public CalculatorStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("o primeiro numero é (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            calculator.FirstNumber = number;
+        }
+
+        [Given("o segundo numero é (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            calculator.SecondNumber = number;
+        }
+
+        [When("os dois numeros sao adicionados")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            result = calculator.Add();
+        }
+
+        [Then("o resultado deve ser (.*)")]
+        public void ThenTheResultShouldBe(int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
